62df2870409d3f8dc5168b9cf8f7e901e2a1981b
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-02-19 07:30:17 +0100

    Handle support for history node states

--- lib/cluster_broker.ex
+++ lib/cluster_broker.ex
@@ -2,50 +2,79 @@ defmodule SmileItDemoUi.ClusterBroker do
     use GenServer
 
     def init(_) do
-        :os.cmd 'epmd -daemon'
         {:ok, {:no_master, %{}}}
     end
     
 
     def start_link() do 
+      :os.cmd 'epmd -daemon'
       Node.start(get_name())
       Node.set_cookie(:marlon_cluster)
       GenServer.start_link(__MODULE__, [], name: :broker_node)
+      SmileItDemoUi.StateStore.start_link()
     end    
 
-    def handle_call(:get_nodes, _from, {master, nodes}) do
+    def handle_call(:restore, _from, {master, nodes}) do
         nodesList = Enum.reduce(nodes, [], fn ({k, v}, acc) -> [%{key: k, node: k, status: v.status, parent: v.parent} | acc] end)
-        {:reply, nodesList, {master, nodes}}
+        states = nodes 
+                 |> Map.keys 
+                 |> Enum.map(fn(n) -> %{name: n, datasets: build_dataset(n)} end) 
+                 |> Enum.reverse
+        {:reply, %{nodes: nodesList, states: states}, {master, nodes}}
     end
 
-    def handle_cast({:join, node, :master}, {_master, nodes}) do
+    def handle_cast({:join, clusterNode, :master}, {_master, nodes}) do
       # broadcast here  
-      IO.puts "Master node #{node}  joined the cluster!"
-      SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "join", %{ "key" => node,"node" => node, "status" => "online" ,"parent" => -1}
-      {:noreply, {node, Map.put_new(nodes, node, %{parent: -1, status: "online"})}}
+      IO.puts "Master node #{clusterNode}  joined the cluster!"
+      SmileItDemoUi.StateStore.add(clusterNode, {DateTime.utc_now(), "Doing nothing",})
+      SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "join", %{ "key" => clusterNode,"node" => clusterNode, "status" => "Doing nothing" ,"parent" => -1}
+      {:noreply, {clusterNode, Map.put_new(nodes, clusterNode, %{parent: -1, status: "online"})}}
     end
 
-    def handle_cast({:join, node, :slave}, {master, nodes}) do
+    def handle_cast({:join, clusterNode, :slave}, {master, nodes}) do
         # broadcast here  
-        IO.puts "Slave node #{node}  joined the cluster!"
-        SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "join", %{ "key" => node,"node" => node, "status" => "online" ,"parent" => master}
-        {:noreply, {master, Map.put_new(nodes, node, %{parent: master, status: "online"})}}
+        IO.puts "Slave node #{clusterNode}  joined the cluster!"
+        SmileItDemoUi.StateStore.add(clusterNode, { DateTime.utc_now(), "Doing nothing"})
+        SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "join", %{ "key" => clusterNode,"node" => clusterNode, "status" => "Doing nothing" ,"parent" => master}
+        {:noreply, {master, Map.put_new(nodes, clusterNode, %{parent: master, status: "online"})}}
       end
 
-    def handle_cast({:leave, node}, {master, nodes}) do
+    def handle_cast({:leave, clusterNode}, {master, nodes}) do
         # broadcast here  
-        IO.puts "Node #{node} leaved the cluster!"
-        SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "leave", %{ "node" => node}
-        {:noreply, {master, Map.delete(nodes, node)}}
+        IO.puts "Node #{clusterNode} leaved the cluster!"
+        SmileItDemoUi.StateStore.delete(clusterNode)
+        SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "leave", %{ "node" => clusterNode}
+        {:noreply, {master, Map.delete(nodes, clusterNode)}}
     end
 
-    def handle_cast({:update, node, status}, {master, nodes}) do
+    def handle_cast({:update, clusterNode, status}, {master, nodes}) do
         # broadcast here  
-        IO.puts "Node #{node} status= #{status} updated!"
-        SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "update", %{ "node" => node, "status" => status}
-        nodes = put_in nodes[node].status, status
+        IO.puts "Node #{clusterNode} status= #{status} updated!"
+        SmileItDemoUi.StateStore.add(clusterNode, {DateTime.utc_now(), status})
+        history = build_dataset(clusterNode)
+        SmileItDemoUiWeb.Endpoint.broadcast "cluster:view", "update", %{ "node" => clusterNode, "status" => status, "history" => history }
+        nodes = put_in nodes[clusterNode].status, status
         {:noreply, {master, nodes}}
-      end
+    end
+
+    defp build_dataset(clusterNode) do
+        [first | states] = clusterNode |> SmileItDemoUi.StateStore.get |> Enum.reverse
+        # IO.inspect first
+        # IO.inspect states
+        build_chart_dataset(states, first, [])
+    end    
+
+    defp build_chart_dataset([], _lastState, ds), do: Enum.reverse(ds)
+    defp build_chart_dataset([{t, _}=current | states], {lt,ls}, ds) do
+        time = DateTime.diff(t, lt)
+        color = get_color(ls)
+        ds = [%{"data" => [time], "backgroundColor" => color} | ds]
+        build_chart_dataset(states, current, ds)
+    end
+
+    defp get_color("Doing nothing"), do: "#d9d9d9"
+    defp get_color("Communicating"), do: "#ffff1a"
+    defp get_color("Processing"), do: "#cc0000"
 
     defp get_name do
         {:ok, [{ {n1, n2, n3, n4}, _ ,_} | _rest]} = :inet.getif()
